a7faa234fcc610f946454c66db34b475fecffc27
Ivan Guajana <ivan.guajana@vilea.ch>
2012-08-15 12:48:47 +0200

    Added documentation example

---
+++ Documentation/Documentation/VIViewController.h
@@ -0,0 +1,31 @@
+//
+//  VIViewController.h
+//  Documentation
+//
+//  Created by Ivan Guajana on 15.08.12.
+//  Copyright (c) 2012 Vilea GmbH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+/** A view controller used to manage the first view */
+@interface VIViewController : UIViewController
+
+/** @name Properties */
+/** The name */
+@property (nonatomic, strong) NSString *name;
+
+/** The address */
+@property (nonatomic, strong) NSString *address;
+
+
+/** @name Actions */
+
+/** Perform an action
+ * @param f The argument
+ */
+- (void)doSomethingWithFloat:(float)f;
+
+/** @return Whether it is visible or not */
+- (BOOL)isVisible;
+@end
---
+++ Documentation/Documentation/VIViewController.m
@@ -0,0 +1,29 @@
+//
+//  VIViewController.m
+//  Documentation
+//
+//  Created by Ivan Guajana on 15.08.12.
+//  Copyright (c) 2012 Vilea GmbH. All rights reserved.
+//
+
+#import "VIViewController.h"
+
+@interface VIViewController ()
+
+@end
+
+@implementation VIViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
